61ae92b8e89007fbe4266b47d522b815131382eb
HimanshuNoida <naudiyalhimanshu@gmail.com>
2023-09-25 10:33:51 +0530

    wromg password

--- target/RunCuke/cucumber.html/report.js
+++ target/RunCuke/cucumber.html/report.js
@@ -59,7 +59,7 @@ formatter.step({
   "rows": [
     {
       "cells": [
-        "Naulikechite@987"
+        "Nlikechite@987"
       ]
     }
   ],
@@ -235,7 +235,8 @@ formatter.match({
   "location": "Searched_item_to_cart.user_enter_the_item_name_in_the_search_bar_and_click_search(String)"
 });
 formatter.result({
-  "status": "passed"
+  "error_message": "org.openqa.selenium.ElementNotInteractableException: element not interactable\n  (Session info: chrome\u003d116.0.5845.188)\nBuild info: version: \u00274.11.0\u0027, revision: \u0027040bc5406b\u0027\nSystem info: os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002717.0.6\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCommand: [f93716a6ee480dd58ac40e5481198fc7, sendKeysToElement {id\u003d0F241C497A8F85DDEC1FA383814439E9_element_308, value\u003d[Ljava.lang.CharSequence;@29c17c3d}]\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 116.0.5845.188, chrome: {chromedriverVersion: 116.0.5845.96 (1a3918166880..., userDataDir: C:\\Users\\Admin\\AppData\\Loca...}, fedcm:accounts: true, goog:chromeOptions: {debuggerAddress: localhost:53055}, networkConnectionEnabled: false, pageLoadStrategy: normal, platformName: windows, proxy: Proxy(), se:cdp: ws://localhost:53055/devtoo..., se:cdpVersion: 116.0.5845.188, setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:extension:minPinLength: true, webauthn:extension:prf: true, webauthn:virtualAuthenticators: true}\nElement: [[ChromeDriver: chrome on windows (f93716a6ee480dd58ac40e5481198fc7)] -\u003e xpath: //input[@id\u003d\u0027twotabsearchtextbox\u0027]]\nSession ID: f93716a6ee480dd58ac40e5481198fc7\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:77)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:499)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:480)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:200)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:133)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:52)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:191)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.invokeExecute(DriverCommandExecutor.java:196)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:171)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:518)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.execute(RemoteWebElement.java:224)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.sendKeys(RemoteWebElement.java:111)\r\n\tat java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:77)\r\n\tat java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.base/java.lang.reflect.Method.invoke(Method.java:568)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:51)\r\n\tat jdk.proxy2/jdk.proxy2.$Proxy27.sendKeys(Unknown Source)\r\n\tat Pages.actions.Add_Item_To_cartActions.enterItemintoSearchBox(Add_Item_To_cartActions.java:32)\r\n\tat steps.Searched_item_to_cart.user_enter_the_item_name_in_the_search_bar_and_click_search(Searched_item_to_cart.java:37)\r\n\tat ✽.user enter the item name \"shoes\" in the search bar and click search(file:src/test/resources/feature/Run_product_into_searchbar.feature:11)\r\n",
+  "status": "failed"
 });
 formatter.step({
   "name": "item should display and",
@@ -245,7 +246,7 @@ formatter.match({
   "location": "Searched_item_to_cart.item_should_display_and()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "user should able to add it to cart",
@@ -255,7 +256,7 @@ formatter.match({
   "location": "Searched_item_to_cart.user_should_able_to_add_it_to_cart()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "Select the address where user want to deleiver",
@@ -265,7 +266,7 @@ formatter.match({
   "location": "Searched_item_to_cart.select_the_address_where_user_want_to_deleiver()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "purchase it after payment perform succesfull",
@@ -275,8 +276,9 @@ formatter.match({
   "location": "Searched_item_to_cart.purchase_it_after_payment_perform_succesfull()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
+formatter.embedding("image/png", "embedded0.png", null);
 formatter.after({
   "status": "passed"
 });
